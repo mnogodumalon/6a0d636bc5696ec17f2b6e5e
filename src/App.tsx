496efdd60e804b8574e29ff9ee3cbe0158543566
Klar <klar@livinglogic.de>
2026-05-20 07:34:35 +0000

    Klar Auto-Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { ErrorBusProvider } from '@/components/ErrorBus';
 import { Layout } from '@/components/Layout';
 import DashboardOverview from '@/pages/DashboardOverview';
-import { WorkflowPlaceholders } from '@/components/WorkflowPlaceholders';
 import AdminPage from '@/pages/AdminPage';
 import FeierDerZahl1Page from '@/pages/FeierDerZahl1Page';
 import PublicFormFeierDerZahl1 from '@/pages/public/PublicForm_FeierDerZahl1';
@@ -26,7 +25,7 @@ export default function App() {
               {/* <public:routes> */}
               {/* </public:routes> */}
               <Route element={<Layout />}>
-                <Route index element={<><div className="mb-8"><WorkflowPlaceholders /></div><DashboardOverview /></>} />
+                <Route index element={<DashboardOverview />} />
                 <Route path="feier-der-zahl-1" element={<FeierDerZahl1Page />} />
                 <Route path="admin" element={<AdminPage />} />
                 {/* <custom:routes> */}
